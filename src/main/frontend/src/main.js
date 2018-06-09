531133fe1b8fd435ecd58ccb842d1262fcb02970
Paweł Urban <urbanpawel@o2.pl>
2018-06-09 11:01:20 +0200

    Możliwość Rejestracji nowych użytkowników dodana

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueResource from "vue-resource";
+//var VueResource = require('vue-resource'); - to nie działa ze względu na różne dialekty frontendu
+
+Vue.use(VueResource);
+
+Vue.http.options.root = '/api';
 
 new Vue({
     el: '#app',
